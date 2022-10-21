1997f3f507b65e8bec12562f471419ad9b60bdbd
Csongor Mikos <csongor.mikos@stud.uni-corvinus.hu>
2022-10-21 08:09:50 +0200

    Dinamikus funkciók

--- 6_gyak_webservice/6_gyak_webservice/Form1.cs
+++ 6_gyak_webservice/6_gyak_webservice/Form1.cs
@@ -20,10 +20,7 @@ namespace _6_gyak_webservice
         public Form1()
         {
             InitializeComponent();
-            ServiceCall();
-            dataGridView1.DataSource = Rates;
-            ProcessXML();
-            BuildChart();
+            RefreshData();
         }
 
         private string ServiceCall()
@@ -31,9 +28,9 @@ namespace _6_gyak_webservice
             var mnbService = new MNBArfolyamServiceSoapClient();
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
 
             var response = mnbService.GetExchangeRates(request);
@@ -81,6 +78,30 @@ namespace _6_gyak_webservice
             chartArea.AxisX.MajorGrid.Enabled = false;
             chartArea.AxisY.MajorGrid.Enabled = false;
             chartArea.AxisY.IsStartedFromZero = false;
+        }
+
+        private void RefreshData()
+        {
+            Rates.Clear();
+            dataGridView1.DataSource = Rates;
+            ServiceCall();
+            ProcessXML();
+            BuildChart();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
         }
     }
 }
