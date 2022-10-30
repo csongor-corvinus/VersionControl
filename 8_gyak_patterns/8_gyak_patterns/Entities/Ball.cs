9730515826c6325b5618c20d9a78d48f390e5c85
Csongor Mikos <csongor.mikos@stud.uni-corvinus.hu>
2022-10-30 17:03:02 +0100

    Futószalag létrehozása

--- 8_gyak_patterns/8_gyak_patterns/Entities/Ball.cs
+++ 8_gyak_patterns/8_gyak_patterns/Entities/Ball.cs
@@ -8,7 +8,7 @@ using System.Windows.Forms;
 
 namespace _8_gyak_patterns.Entities
 {
-    class Ball:Label
+    public class Ball:Label
     {
         public Ball()
         {
--- 8_gyak_patterns/8_gyak_patterns/Entities/BallFactory.cs
+++ 8_gyak_patterns/8_gyak_patterns/Entities/BallFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace _8_gyak_patterns.Entities
 {
-    class BallFactory
+    public class BallFactory
     {
         public Ball CreateNew()
         {
