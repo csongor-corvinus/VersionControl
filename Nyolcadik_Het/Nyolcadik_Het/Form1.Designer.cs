057230bec057b58ba0b4c1ed0252dc84b907f463
Csongor Mikos <csongor.mikos@stud.uni-corvinus.hu>
2022-11-08 11:23:21 +0100

    Fájlba mentés

--- Nyolcadik_Het/Nyolcadik_Het/Form1.Designer.cs
+++ Nyolcadik_Het/Nyolcadik_Het/Form1.Designer.cs
@@ -31,6 +31,7 @@ namespace Nyolcadik_Het
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
@@ -55,11 +56,22 @@ namespace Nyolcadik_Het
             this.dataGridView2.Size = new System.Drawing.Size(726, 412);
             this.dataGridView2.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(732, 431);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Mentés";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1562, 722);
+            this.ClientSize = new System.Drawing.Size(1562, 519);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
@@ -74,6 +86,7 @@ namespace Nyolcadik_Het
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
