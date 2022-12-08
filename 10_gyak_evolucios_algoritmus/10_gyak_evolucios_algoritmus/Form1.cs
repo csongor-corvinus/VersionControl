6c3d7d430f877b1f3f1b948a17776a232f5ae669
Csongor Mikos <csongor.mikos@stud.uni-corvinus.hu>
2022-12-08 10:47:49 +0100

    A játék kipróbálása

--- 10_gyak_evolucios_algoritmus/10_gyak_evolucios_algoritmus/Form1.cs
+++ 10_gyak_evolucios_algoritmus/10_gyak_evolucios_algoritmus/Form1.cs
@@ -21,6 +21,9 @@ namespace _10_gyak_evolucios_algoritmus
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
         }
     }
 }
